8501915d6b48bb5da2f4ae9dd8bbdb2934772b2b
Bernadette92 <79193891+Bernadette92@users.noreply.github.com>
2021-03-22 14:32:13 +0100

    Add files via upload

---
+++ 30_ Opdracht-Iterators/script.js
@@ -0,0 +1,17 @@
+const colours = ["yellow", "blue", "red", "orange"];
+
+/*
+var i = 0;
+while (i < colours.length) {
+  console.log(colours[i]);
+  i++;
+}
+
+for (i = 0; i < colours.length; i++) {
+  console.log(colours[i]);
+}
+*/
+
+colours.forEach(function (colours) {
+  console.log(colours);
+});
